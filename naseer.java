76b11583d83e4a89aa04dd905a830b9ce9581a8f
naseerahammed <naseerahammed19@gmail.com>
2024-09-18 12:39:15 +0530

    adding java file

---
+++ naseer.java
@@ -0,0 +1,10 @@
+class fourthprogram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+        System.out.println("world!");
+        System.out.print("Hi mom ");
+        System.out.print("Hi dad!");
+    }
+}
